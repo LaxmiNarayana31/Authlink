756088968ff35f1273b06cc8b9fb0a52b650b0fb
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-08-28 22:41:10 +0530

    get authenticated user from the request

--- app/helper/email_sender.py
+++ app/helper/email_sender.py
@@ -1,8 +1,13 @@
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from requests import Session
+from app.auth.jwt_handler import decode_jwt_token
 from app.helper.email_config import email_settings
+from fastapi import Depends, HTTPException, Header, Request
 import smtplib
 import random
+from app.models.user_model import UserModel
+from config.database import get_db
 
 
 class Helper:
@@ -60,4 +65,28 @@ class Helper:
             
         except Exception as e:
             print(f"Failed to send email to {receiver_email}: {str(e)}")
+    
 
+    # get authenticated user from the request
+    def getAuthUser(request: Request, db: Session = Depends(get_db)):
+        try:
+            authorization: str = request.headers.get("Authorization")
+            
+            if not authorization or not authorization.startswith("Bearer "):
+               raise HTTPException(status_code=401, detail="Authorization header missing")
+
+            token = authorization.split(" ")[1]
+            
+            email = decode_jwt_token(token)
+            if not email:
+                raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+            user = db.query(UserModel).filter(UserModel.email == email).first()
+
+            if not user:
+                raise HTTPException(status_code=404, detail="User not found")
+
+            return user
+        except Exception as e:
+            print("Exception occurred:", str(e))
+    
