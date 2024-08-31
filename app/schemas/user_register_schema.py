e057523cacdbf477720836da8726818b6884bc39
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-08-31 21:21:42 +0530

    register schema

---
+++ app/schemas/user_register_schema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class UserRegisterSchema(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    role_id: Optional[int] = None
+    city: Optional[str] = None
+    state: Optional[str] = None
+    country: Optional[str] = None
+
+    class Config():
+        from_attributes = True
